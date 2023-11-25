bf62a56081d024d64c0e7f8feabe93931c6efe78
radosan <radoslaw.sanetra@gmail.com>
2023-11-25 18:02:28 +0100

    checked dependency of accuracy from number of trees

--- Random Forest from Scratch/task/Student.py
+++ Random Forest from Scratch/task/Student.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
@@ -17,10 +18,10 @@ class RandomForestClassifier:
         self.is_fit = False
 
     def fit(self, X_train, y_train):
-        for _ in trange(self.n_trees):
+        for _ in range(self.n_trees):
             x_s, y_s = self.create_bootstrap(X_train, y_train)
             tree = DecisionTreeClassifier(max_features='sqrt', max_depth=self.max_depth,
-                                          min_impurity_decrease=self.min_error, random_state=52)
+                                          min_impurity_decrease=self.min_error, random_state=None)
             tree.fit(x_s, y_s)
             self.forest.append(tree)
         self.is_fit = True
@@ -38,6 +39,15 @@ class RandomForestClassifier:
         return bx[mask], by[mask]
 
 
+def plot_acc(acc):
+    fig, ax = plt.subplots()
+    fig.suptitle('Random forest dependence of accuracy from the number of trees')
+    ax.set_xlabel('number of trees')
+    ax.set_ylabel('model accuracy')
+    ax.plot(np.arange(1, len(acc)+1), acc, '-dg')
+    plt.show()
+
+
 if __name__ == '__main__':
     data = pd.read_csv('https://www.dropbox.com/s/4vu5j6ahk2j3ypk/titanic_train.csv?dl=1')
 
@@ -52,8 +62,12 @@ if __name__ == '__main__':
     X['Embarked'].replace({'S': 0, 'C': 1, 'Q': 2}, inplace=True)
 
     X_train, X_val, y_train, y_val = train_test_split(X.values, y.values, stratify=y,
-                                                      train_size=0.8, random_state=52)
+                                                      train_size=0.8, random_state=None)
 
-    forest = RandomForestClassifier(n_trees=300, max_depth=100, min_error=1e-4).fit(X_train, y_train)
-    prediction = forest.predict(X_val)
-    print(round(accuracy_score(y_val, prediction), 3))
+    accuracy = []
+    for n in trange(1, 601):
+        forest = RandomForestClassifier(n_trees=n).fit(X_train, y_train)
+        prediction = forest.predict(X_val)
+        accuracy.append(round(accuracy_score(y_val, prediction), 3))
+    print(accuracy[:20])
+    plot_acc(accuracy)
